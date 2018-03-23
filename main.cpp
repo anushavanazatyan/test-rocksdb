26ebf73a67f6d86774c3a9c4d1b3b27a8a87e529
anushavanazatyan <anushavanazatyan@gmail.com>
2018-03-24 03:39:27 +0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <QCoreApplication>
+#include <assert.h>
+#include "rocksdb/db.h"
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    rocksdb::DB* db;
+    rocksdb::Options options;
+    options.create_if_missing = true;
+    rocksdb::Status status = rocksdb::DB::Open(options, "./tmp/testdb", &db);
+  //  assert(status.ok());
+
+    options.error_if_exists = true; // - rise error if db exists
+    delete db;
+    int bb;
+    printf("everything ok ");
+    scanf(" press key %d", &bb);
+    std::string value = std::string(" Some Value");
+    std::string key = std::string("testkey");
+
+    rocksdb::Status sss = db->Put(rocksdb::WriteOptions(), key, value);
+    if(sss.ok())
+    {
+        printf("successed to create and write in base");
+    }
+    return a.exec();
+}
